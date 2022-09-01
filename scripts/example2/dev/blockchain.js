c219838317c1f2f57446707196eb59fadc0d2522
nikenaml <niken.amelia123@gmail.com>
2022-09-01 09:40:06 +0700

    create add newBlock

--- scripts/example2/dev/blockchain.js
+++ scripts/example2/dev/blockchain.js
@@ -18,6 +18,26 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
         transactions: this.newTransactions,
 
         // Basically, nonce comes from a proof of work and a nonce in our case is simply just a number. Could be any number could be to ten, ten thousand one hundred thousand. It doesn't really matter. But this Nonce is pretty much a proof. That we created this new black in a legitimate way by using a proof of work method.
-        nonce: nonce
+        nonce: nonce,
+
+        // Our next property is going to be a hash, and this hash is simply going to be equal to the hash that we pass into our function up here or into our create new block method up here. And basically, this hash will be the data. From our new block. So basically, what's going to happen is we are going to pass our transactions or our new transactions into a hashing function. Basically, what that means is all of our transactions are going to be basically compressed into a single string of code, a single string. And that is what will be our hash.
+        hash: hash, 
+        // So they're both hashes, this is the hash of the current block and this is the hash of the previous
+
+        // And this is very similar to our hash except our hash right here. This is the data from our current black hacked into a string. And this previous block hash right here is the data from our previous black or the previous black to this one, hacked into a string.
+        previousBlockHash: previousBlockHash
     };
+
+    // We are putting all of the new transactions into this block, so we want to clear out this entire new transactions array so we can start over for the next block.
+    this.newTransactions = [];
+
+    // This basically simply takes this new block that we have created and pushes it into our chain and adds it to our chain.
+    this.chain.push(newBlock);
+
+    return newBlock;
+
+    // summary: So basically, what our new create new block method does on a high level is it creates a new block.
+    // And inside of this block, we have our transactions, the new transactions that have that have been created since our last block was mined.
+    // And after we create a new block, we clear out the new transactions, we push the new block into our chain and we simply return our new block from this method.
+
 }
