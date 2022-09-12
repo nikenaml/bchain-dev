b5799e9f19f6fdaeb8db501622aa50a3e4a898d5
nikenaml <niken.amelia123@gmail.com>
2022-09-12 11:59:40 +0700

    add newblock data w/ sample data

--- scripts/example2/dev/test.js
+++ scripts/example2/dev/test.js
@@ -2,4 +2,9 @@ const Blockchain = require("./blockchain")
 
 const bitcoin = new Blockchain();
 
+bitcoin.createNewBlock(2389, 'QWERTYUIOP098','POIUYTREWQ1233');
+bitcoin.createNewBlock(111, 'asdfghjkl098','lkjhgfdsa123');
+bitcoin.createNewBlock(1909, 'ZXCVBNM456','MNBVCXZ654');
+
+
 console.log(bitcoin);
