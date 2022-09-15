52ed8fc7b17f4d25ca22fb6c204a8d022b751b33
nikenaml <niken.amelia123@gmail.com>
2022-09-15 14:13:10 +0700

    create schema api for 3 endpoint

--- scripts/example2/dev/api.js
+++ scripts/example2/dev/api.js
@@ -4,8 +4,20 @@ const app = express()
 // We can handle different end points or different routes.
 // For example, we have a get endpoint, which is just slash. And with this endpoint, we are sending back the response of Hello World and then this whole server is listening on Port 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get('/blockchain', function (req, res) {
+  
+});
 
-app.listen(3000)
+app.post('/transaction', function (req, res) {
+
+});
+
+// this mine endpoint will do is it will mine a new block for us or create a new block for us,
+app.get('/mine', function (req, res) {
+
+});
+
+app.listen(3000, function(){
+    // the reason that we do this is just so that when our port is actually running, we will know that because we'll see this text.
+    console.log('Listening on port 3000...');
+});
