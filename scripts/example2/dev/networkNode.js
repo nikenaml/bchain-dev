06ee87c48bbef2b60b9b93d2a2d89b85aaebbdf2
nikenaml <niken.amelia123@gmail.com>
2022-09-18 07:03:46 +0700

    create register&broandcast mesthod

--- scripts/example2/dev/networkNode.js
+++ scripts/example2/dev/networkNode.js
@@ -61,6 +61,34 @@ app.get('/mine', function (req, res) {
 // register a node and broadcast it the network
 app.post('/register-and-broadcast-node', function(req,res) {
     const newNodeUrl = req.body.newNodeUrl;
+	if (bitcoin.networkNodes.indexOf(newNodeUrl) == -1) bitcoin.networkNodes.push(newNodeUrl);
+
+	const regNodesPromises = [];
+	bitcoin.networkNodes.forEach(networkNodeUrl => {
+		const requestOptions = {
+			uri: networkNodeUrl + '/register-node',
+			method: 'POST',
+			body: { newNodeUrl: newNodeUrl },
+			json: true
+		};
+
+		regNodesPromises.push(rp(requestOptions));
+	});
+
+	Promise.all(regNodesPromises)
+	.then(data => {
+		const bulkRegisterOptions = {
+			uri: newNodeUrl + '/register-nodes-bulk',
+			method: 'POST',
+			body: { allNetworkNodes: [ ...bitcoin.networkNodes, bitcoin.currentNodeUrl ] },
+			json: true
+		};
+
+		return rp(bulkRegisterOptions);
+	})
+	.then(data => {
+		res.json({ note: 'New node registered with network successfully.' });
+	});
 });
 
 // register a node with the network
